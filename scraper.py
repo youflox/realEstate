12f0258426c80d284a780e644d52d676d036f495
unknown <youflox@gmail.com>
2020-08-16 07:07:51 +0530

    readme pending

--- scraper.py
+++ scraper.py
@@ -1,26 +1,45 @@
 import requests
 from bs4 import BeautifulSoup as soup
 
-# Web sites
-liveMint = ['https://www.livemint.com/topic/real-estate']
-realty = 'https://realty.economictimes.indiatimes.com/latest-news'  # id = id="recent-news-listing"
-magicBricks = 'https://content.magicbricks.com/property-news/pune-real-estate-news-industry-news'  # leftContainer
-
-urls = {
-    # 'name' : [ 'url', 'main_class_to_search_on_website', ]
-
-    'liveMint': [liveMint, 'listing']
-
-}
-
-
-def real_estate_stories(url, mainClass, ):
-    request = requests.get(liveMint)
+# arguments order as follows -- > func(URL, class_of_newsBody, args->(sub classes), kw->(tagType =[tag name, property ]
+def real_estate_stories(url, main_class, *args, **kwargs):
+    request = requests.get(url)
     page = soup(request.content, 'html.parser')
-    allHeadlines = page.find_all(class_='listing')
 
-    for headline in allHeadlines:
-    headlineTitle = headline.find('meta', itemprop="name")
-    headlineURL = headline.find('meta', itemprop="url")
+    all_headlines = page.find(class_=main_class)
 
-    print((headlineTitle)['content'] + " : " + (headlineURL)['content'])
+    if args:
+        for arg in args:
+            print(arg)
+            all_headlines = [headline.find(class_=arg) for headline in all_headlines]
+
+    if kwargs:
+        for key, values in kwargs.items():
+            if key == 'meta':
+                all_headlines = all_headlines.find_all(key, itemprop=values)
+                all_headlines = [heads['content']  for heads in all_headlines]
+
+            # defining Explicitly for "a" tags
+            if key == 'tag' and values == 'a':
+                all_headlines = all_headlines.find_all('a')
+                all_headlines = [heads['href'] for heads in all_headlines]
+
+            # defining for all other tags except "a"
+            if key == 'tag' and values != 'a':
+                all_headlines = all_headlines.find_all(values)
+
+    # Final output
+    for url in all_headlines:
+        if 'https' in url:
+            print(url)
+
+
+# New web sites cab be added here (direct link to latest news)
+liveMint = 'https://www.livemint.com/topic/real-estate'
+realty = 'https://realty.economictimes.indiatimes.com/latest-news'
+magicBricks = 'https://content.magicbricks.com/property-news/pune-real-estate-news-industry-news'
+
+
+real_estate_stories(liveMint, 'listView', meta=['url'])
+real_estate_stories(realty, 'wdgt', tag='a')
+real_estate_stories(magicBricks, 'leftContainer', tag='a')
